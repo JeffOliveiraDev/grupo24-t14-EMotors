fbf85ade6a686ef7850a792a6a7fd15b9a443f2d
Jean Souza <jeanhsouza@gmail.com>
2023-06-05 18:38:25 -0300

    feat: CreateEntityDTO User&Announce

--- emotors/src/modules/announcement/entities/announcement.entity.ts
+++ emotors/src/modules/announcement/entities/announcement.entity.ts
@@ -1,1 +1,13 @@
-export class Announcement {}
+export class Announcement {
+    readonly id: string;
+    model: string;
+    fuel: string;
+    mileage: string;
+    color: string;
+    pfipe: boolean;
+    sellPrice: number;
+    description: string;
+    coverImage: string;
+    detailsImage: string;
+    
+}
--- emotors/src/modules/users/entities/user.entity.ts
+++ emotors/src/modules/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { randomUUID } from "node:crypto";
+import { Exclude } from "class-transformer";
+
+export class User {
+    readonly id: string;
+    name: string;
+    email: string;
+    cpf: string;
+    telephone: string;
+    birthdate: Date;
+    description: string;    
+
+    @Exclude()
+    password: string;
+
+    constructor() {
+        this.id = randomUUID();
+    }
+}
