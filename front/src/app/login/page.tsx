dee73d1fedbb1ed2b81e6c2a273600777f64dce2
Jefferson de Barros Oliveira <102183069+JeffOliveiraDev@users.noreply.github.com>
2023-06-09 08:51:34 -0300

    feat: folder organization

---
+++ front/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Teste from "../../components/card/teste";
+
+const loginPage = () => {
+  return (
+    <>
+      <h2>Login</h2>
+      <Teste />
+    </>
+  );
+};
+
+export default loginPage;
